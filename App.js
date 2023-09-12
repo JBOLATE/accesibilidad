4e5600193edadb4f0eccc177665d1daced00229d
joaquin <joaquinolate36@gmail.com>
2023-09-12 20:41:30 -0300

    cambio de rama

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> joaquin gay</Text>
+      <Text> a tu waaa </Text>
       <StatusBar style="auto" />
     </View>
   );
