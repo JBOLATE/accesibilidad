95f92e3a28c59361458cd075684c884e1b8f944d
joaquin <joaquinolate36@gmail.com>
2023-10-01 13:50:43 -0300

    probando

--- App.js
+++ App.js
@@ -4,11 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> voten a milei, porque masa le dan a tu waaa </Text>
+      <Text> voten a milei, porque masa le dan a tu waaa</Text>
       <StatusBar style="auto" />
+      
+
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
