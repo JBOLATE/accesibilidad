c6ed8707494d56bc9db7cb78c39ffc485731b7fb
joaquin <joaquinolate36@gmail.com>
2023-10-01 14:25:52 -0300

    iniciando inicio

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> voten a milei, porque masa le dan a tu waaa</Text>
+      <Text style={styles.titulo}>FarmaNeuquen</Text>
+      <Text style={styles.titulo}>sign in to your account</Text>
+      <TextInput
+      placeholder='franco@gmail.com'
+      />
+      <TextInput
+      placeholder='password'
+      />
       <StatusBar style="auto" />
-      
-
     </View>
   );
 }
@@ -16,8 +21,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center', 
   },
+titulo: {
+  fontSize: 30,
+  
+}
+
 });
